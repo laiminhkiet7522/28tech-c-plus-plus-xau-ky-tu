a6cec9f26334d2e0841ec10985096230673ccca1
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-05-12 09:53:16 +0700

    Kiem tra xau Pangram cua mot chuoi in thuong hoac in hoa

---
+++ Kiem-tra-xau-pangram-1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+//Xau pangram la xau co day du ky tu tu a->z
+int pangram(char c[])
+{
+    int cnt[26]= {0};
+    for(int i=0; i<strlen(c); i++)
+    {
+        cnt[c[i]-'a']=1;
+    }
+    for(int i=0; i<26; i++)
+    {
+        if(cnt[i]==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char c[100];
+    cout<<"Nhap chuoi: ";
+    cin.getline(c,100);
+    if(pangram(c))
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
